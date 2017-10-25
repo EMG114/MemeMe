c35fd9e1bd2fc421f46b79e505fbefe9fbc1dad8
EMG114 <egutierrez411@gmail.com>
2017-10-25 18:52:24 -0400

    added cell, details and collection view

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,24 @@
+//
+//  Meme.swift
+//  MemeApp
+//
+//  Created by Erica on 10/9/17.
+//  Copyright © 2017 Erica Gutierrez. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    
+    var topText: String
+    var bottomText: String
+    var originalMeme: UIImage
+    var memedImage:UIImage
+    
+}
+    
+  
+    
+
---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Erica on 10/25/17.
+//  Copyright © 2017 Erica Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+}
